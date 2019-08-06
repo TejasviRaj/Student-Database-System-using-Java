58c8b0fc2657689c38bf48c0584ac825cfb0d419
Tejasvi <tejpant@gmail.com>
2019-08-06 15:12:28 +0545

    transferred to EADD-maven

--- EADD-maven/EADD-maven/Java/com/tejasvi/web/Home.java
+++ EADD-maven/EADD-maven/Java/com/tejasvi/web/Home.java
@@ -1,39 +1,113 @@
 package com.tejasvi.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Home
  */
 public class Home extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Home() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static final String NULL = null;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String name=request.getParameter("username");
+		String pass=request.getParameter("userpass");
+		
+//		if(LoginDao.validate(n, p)){
+//			RequestDispatcher rd=request.getRequestDispatcher("servlet2");
+//			rd.forward(request,response);
+//		}
+		
+		if(name.equals("admin")&& pass.equals("admin")){
+			
+	        HttpSession session = request.getSession();
+	        session.setAttribute("name", name);
+	        
+//			RequestDispatcher rd=request.getRequestDispatcher("servlet1");
+//			rd.forward(request,response);
+	        
+	        doGet(request,response);
+		
+		}
+		
+		else{
+			out.print("Sorry username or password error");
+			RequestDispatcher rd=request.getRequestDispatcher("index.html");
+			rd.include(request,response);
+		}
+		
+		out.close();
 	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+//		HttpSession session = request.getSession();
+//        String name= (String) session.getAttribute("name");
+//        
+//        if (name.contentEquals("admin"))
+//    	{
+//    		RequestDispatcher rd=request.getRequestDispatcher("index.html");
+//    		rd.forward(request,response);
+//    	}
+//    	else
+//    	{
+//    		RequestDispatcher rd=request.getRequestDispatcher("index.html");
+//    		rd.forward(request,response);	
+//    		}
+		
+		
+		PrintWriter out = response.getWriter();
+
+		String name;
+		HttpSession session = request.getSession();
+		
+		try
+		{
+			name=(String) session.getAttribute("name");
+
+		}
+		
+		catch(Exception e)
+		{
+			name=NULL;
+
+		}
+		
+		
+		if (name != NULL)
+		{
+			if (name.contentEquals("admin"))
+			{
+				RequestDispatcher rd= request.getRequestDispatcher("mainScreen.jsp");
+				response.setContentType("text/html");
+				rd.forward(request,response);
+				
+//				String n=request.getParameter("username");
+//				out.print("aaija ");
+//				
+//				out.close();
+			}
+			
+		}
+		else
+		{
+			out.print("You must login first");
+			response.sendRedirect("index.html");  
+		}
+        
+	}
+	
 
 }
