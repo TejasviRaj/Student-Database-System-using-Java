b655150df9bcfc833b38006bb312c6cfb004ba88
Tejasvi <tejpant@gmail.com>
2019-08-06 19:11:25 +0545

    student.java started

--- EADD-maven/EADD-maven/Java/com/tejasvi/web/Home.java
+++ EADD-maven/EADD-maven/Java/com/tejasvi/web/Home.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import com.tejasvi.web.*;
 
 /**
  * Servlet implementation class Home
@@ -18,7 +19,6 @@ public class Home extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		
---
+++ EADD-maven/EADD-maven/Java/com/tejasvi/web/Student.java
@@ -0,0 +1,11 @@
+package com.tejasvi.web;
+
+public class Student {
+	private int id;
+	private String student_Name;
+	private int Grade;
+	private int Roll_no;
+	private String Gender;
+	private String Address;
+	
+}
