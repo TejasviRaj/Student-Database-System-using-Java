b842a1455e94652516d0b82d4c255d3bc409a0a7
Tejasvi <tejpant@gmail.com>
2019-08-08 01:54:12 +0545

    table added

--- EADD-maven/EADD-maven/src/main/webapp/trp.js
+++ EADD-maven/EADD-maven/src/main/webapp/trp.js
@@ -1,3 +1,7 @@
+var myObj;
+
+getStudent();
+
 function addStudent()
 {
 	alert("start");
@@ -15,7 +19,6 @@ function addStudent()
 
 	xhr.addEventListener("readystatechange", function () {
 	  if (this.readyState === this.DONE) {
-	    document.getElementById('para').innerHTML=this.responseText;
 	  }
 	});
 
@@ -26,4 +29,91 @@ function addStudent()
 	alert("end");
 
 return false;
+}
+
+function getStudent()
+{
+	var data = null;
+
+	var xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
+
+	xhr.addEventListener("readystatechange", function () {
+	  if (this.readyState === this.DONE) {
+		    myObj = JSON.parse(this.responseText);
+		    generateTable();
+	  }
+	});
+
+	xhr.open("GET", "http://localhost:8080/EADD-maven/GetStudent");
+
+	xhr.send(data);	
+}
+
+function generateTable()
+
+{
+
+
+   var tbody =document.getElementById("table_body");
+
+var child = tbody.lastElementChild;
+while (child) {
+tbody.removeChild(child);
+child = tbody.lastElementChild;
+}
+
+for (i = 0; i < myObj.length; i++) {
+
+var row = document.createElement("tr");
+
+var cell = document.createElement("td");
+
+cell.setAttribute("class", "lalign");
+
+var cellText = document.createTextNode(myObj[i]["student_Name"]);
+
+cell.appendChild(cellText);
+
+row.appendChild(cell);
+
+cell = document.createElement("td");
+
+cellText = document.createTextNode(myObj[i]["Grade"]);
+cell.appendChild(cellText);
+
+row.appendChild(cell);
+
+
+cell = document.createElement("td");
+
+cellText = document.createTextNode(myObj[i]["Roll_no"]);
+cell.appendChild(cellText);
+
+row.appendChild(cell);
+
+
+cell = document.createElement("td");
+
+cellText = document.createTextNode(myObj[i]["Gender"]);
+cell.appendChild(cellText);
+
+row.appendChild(cell);
+
+cell = document.createElement("td");
+
+cellText = document.createTextNode(myObj[i]["Address"]);
+cell.appendChild(cellText);
+
+row.appendChild(cell);
+
+
+
+tbody.appendChild(row);
+}
+
+
+
+    document.getElementById('para').innerHTML=myObj[0]["student_Name"];
+
 }
