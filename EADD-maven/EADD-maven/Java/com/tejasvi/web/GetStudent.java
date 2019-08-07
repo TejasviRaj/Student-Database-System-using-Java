278761b1c62ce3dd9e3bad0cedc39c0cedc58676
Tejasvi <tejpant@gmail.com>
2019-08-07 22:05:41 +0545

    get json done

--- EADD-maven/EADD-maven/Java/com/tejasvi/web/GetStudent.java
+++ EADD-maven/EADD-maven/Java/com/tejasvi/web/GetStudent.java
@@ -3,6 +3,7 @@ package com.tejasvi.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,10 +15,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
+
 /**
  * Servlet implementation class GetStudent
  */
 public class GetStudent extends HttpServlet {
+	List<Student> students_list;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -26,6 +29,7 @@ public class GetStudent extends HttpServlet {
 		EntityManager em=emf.createEntityManager();
 		Student s=em.find(Student.class,1);
 		Student t=em.find(Student.class,2);
+		students_list=em.createQuery("SELECT e FROM Student e").getResultList();
 		
 		ArrayList<Student> student_list =new ArrayList<Student>();
 		
@@ -35,7 +39,7 @@ public class GetStudent extends HttpServlet {
 
 		PrintWriter out=response.getWriter();
 		Gson gson= new Gson();
-	    out.println(gson.toJson(student_list));	}
+	    out.println(gson.toJson(students_list));	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
